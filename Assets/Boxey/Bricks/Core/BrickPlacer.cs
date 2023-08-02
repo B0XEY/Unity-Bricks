c518235a18c5dfac5269addb9b74f1266ce402b0
B0XEY <gramcooker@gmail.com>
2023-08-02 15:41:47 -0500

    Scripts

--- Assets/Boxey/Bricks/Core/BrickPlacer.cs
+++ Assets/Boxey/Bricks/Core/BrickPlacer.cs
@@ -1,7 +1,8 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Boxey.Bricks.Core {
     public class BrickPlacer : MonoBehaviour {
-        
+        private List<Vector3> m_brickPoints;
     }
 }
