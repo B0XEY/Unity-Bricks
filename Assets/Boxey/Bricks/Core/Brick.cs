284cb48239623567dda25844e13f60134547a7b3
B0XEY <gramcooker@gmail.com>
2023-08-02 23:11:13 -0500

    fixxing

--- Assets/Boxey/Bricks/Core/Brick.cs
+++ Assets/Boxey/Bricks/Core/Brick.cs
@@ -72,36 +72,42 @@ namespace Boxey.Bricks.Core {
                 8, 10, 9,
                 9, 10, 11,
                 // Right X
-                12, 14, 13,
-                13, 14, 15,
+                13, 14, 12,
+                14, 15, 12,
                 // Top
                 16, 18, 17,
                 17, 18, 19,
-                // Bottom X
-                20, 22, 21,
-                21, 22, 23
+                // Bottom
+                20, 21, 22,
+                22, 21, 23
             };
             var normals = new Vector3[24] {
+                // Front face
                 Vector3.forward,
                 Vector3.forward,
                 Vector3.forward,
                 Vector3.forward,
+                // Back face
                 Vector3.back,
                 Vector3.back,
                 Vector3.back,
                 Vector3.back,
+                // Left face
                 Vector3.left,
                 Vector3.left,
                 Vector3.left,
                 Vector3.left,
+                // Right face
                 Vector3.right,
                 Vector3.right,
                 Vector3.right,
                 Vector3.right,
+                // Top face
                 Vector3.up,
                 Vector3.up,
                 Vector3.up,
                 Vector3.up,
+                // Bottom face
                 Vector3.down,
                 Vector3.down,
                 Vector3.down,
